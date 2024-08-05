2c3c24484173e54a1be9f9c941001b4919a7ee5f
Sayan Sadhu <sayan.sadhu@webspiders.com>
2024-08-05 16:48:48 +0530

    update

--- application/modules/admin/controllers/Discount.php
+++ application/modules/admin/controllers/Discount.php
@@ -115,10 +115,10 @@ class Discount extends CI_Controller {
 
     public function download_excel()
     {
-        $file_url =base_url('assets/csv/select_bulk_materials.xlsx');
-        echo $file_url; die;
+        $file_url = base_url('assets/csv/select_bulk_materials.xlsx');
+        // echo $file_url; die;
         header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: Binary"); 
+        // header("Content-Transfer-Encoding: Binary"); 
         header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
         readfile($file_url);
     }
